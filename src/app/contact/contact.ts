d49960a0bc7d1e5b9b56a4cdfac1767e9ce949b5
chandureddy376 <chandureddy0301@gmail.com>
2025-08-02 21:12:00 +0530

    css changes

--- src/app/contact/contact.ts
+++ src/app/contact/contact.ts
@@ -3,14 +3,23 @@ import { ButtonModule } from 'primeng/button';
 import { InputGroupModule } from 'primeng/inputgroup';
 import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import emailjs from 'emailjs-com';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-contact',
-  imports: [ButtonModule, InputGroupModule, InputGroupAddonModule, RouterModule],
+  imports: [ButtonModule, InputGroupModule, InputGroupAddonModule, RouterModule,FormsModule,ToastModule],
   templateUrl: './contact.html',
-  styleUrl: './contact.css'
+  styleUrl: './contact.css',
+  providers: [MessageService]
 })
 export class Contact {
+   constructor(private messageService: MessageService) {}
+
+
+  enquiredMail:string = '';
 
   @Output() sectionNavigate = new EventEmitter<string>();
 
@@ -22,5 +31,36 @@ export class Contact {
       window.scrollTo({ top: y, behavior: 'smooth' });
     }
   }
+
+  sendEmail() {
+    console.log(this.enquiredMail)
+  if (!this.isValidEmail(this.enquiredMail)) {
+     this.messageService.add({ severity: 'error', summary: 'Email', detail: 'Please a Valid E-Mail id' });
+    return;
+  }
+
+    const templateParams = {
+      user_email: this.enquiredMail,
+      name:this.enquiredMail,
+      title:'Protfolio Enquiry'
+    };
+    console.log(templateParams)
+    emailjs .send(
+        'service_rk32sy8',
+        'template_4waj1af',
+        templateParams,
+        'zofwWtiXDqaNQCCdM'
+      ) .then(() => {
+          this.messageService.add({ severity: 'success', summary: 'Email Received', detail: 'Thanks! Your Mail was received successfully.' });
+          this.enquiredMail = '';
+      }) .catch((error) => {
+         this.messageService.add({ severity: 'error', summary: 'Email error', detail: 'Sending failed. Try again.' });
+      });
+  }
+
+  isValidEmail(email: string): boolean {
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return emailRegex.test(email);
+} 
 
 }
