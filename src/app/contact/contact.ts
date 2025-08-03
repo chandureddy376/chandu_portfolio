ef895873903d6c22a9f1fb6d0958fb247e34dd45
chandureddy376 <chandureddy0301@gmail.com>
2025-08-03 13:03:58 +0530

    hire me form completed

--- src/app/contact/contact.ts
+++ src/app/contact/contact.ts
@@ -7,21 +7,60 @@ import { FormsModule } from '@angular/forms';
 import emailjs from 'emailjs-com';
 import { ToastModule } from 'primeng/toast';
 import { MessageService } from 'primeng/api';
+import { DialogModule } from 'primeng/dialog';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [ButtonModule, InputGroupModule, InputGroupAddonModule, RouterModule,FormsModule,ToastModule],
+  imports: [ButtonModule, InputGroupModule, InputGroupAddonModule, RouterModule, FormsModule, ToastModule, DialogModule, InputTextModule, InputNumberModule, ReactiveFormsModule],
   templateUrl: './contact.html',
   styleUrl: './contact.css',
   providers: [MessageService]
 })
 export class Contact {
-   constructor(private messageService: MessageService) {}
+  constructor(private messageService: MessageService, private fb: FormBuilder) { }
 
-
-  enquiredMail:string = '';
+  isVisibleHireMe: boolean = false;
+  hireForm!: FormGroup;
+  enquiredMail: string = '';
 
   @Output() sectionNavigate = new EventEmitter<string>();
+
+  ngOnInit() {
+    this.hireForm = this.fb.group({
+      username: ['', Validators.required, Validators.pattern(/^[a-zA-Z\s]{2,50}$/)],
+      number: [null, [Validators.required, Validators.pattern(/^\d{10}$/)]],
+      email: ['', [Validators.required, Validators.email, Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)]]
+    });
+  }
+
+
+  openHireModal() {
+    this.isVisibleHireMe = true;
+  }
+
+  submitHireForm() {
+    if (this.hireForm.invalid) {
+      this.hireForm.markAllAsTouched();
+      return;
+    }
+
+    console.log('Form Data:', this.hireForm.value);
+
+    // Optionally send data using EmailJS, API, etc.
+
+    this.isVisibleHireMe = false;
+    this.hireForm.reset();
+  }
+
+  closeHireModal() {
+    this.isVisibleHireMe = false;
+    this.hireForm.reset();
+
+  }
 
   navigateTo(sectionId: string) {
     const el = document.getElementById(sectionId);
@@ -34,33 +73,33 @@ export class Contact {
 
   sendEmail() {
     console.log(this.enquiredMail)
-  if (!this.isValidEmail(this.enquiredMail)) {
-     this.messageService.add({ severity: 'error', summary: 'Email', detail: 'Please a Valid E-Mail id' });
-    return;
-  }
+    if (!this.isValidEmail(this.enquiredMail)) {
+      this.messageService.add({ severity: 'error', summary: 'Email', detail: 'Please a Valid E-Mail id' });
+      return;
+    }
 
     const templateParams = {
       user_email: this.enquiredMail,
-      name:this.enquiredMail,
-      title:'Protfolio Enquiry'
+      name: this.enquiredMail,
+      title: 'Protfolio Enquiry'
     };
     console.log(templateParams)
-    emailjs .send(
-        'service_rk32sy8',
-        'template_4waj1af',
-        templateParams,
-        'zofwWtiXDqaNQCCdM'
-      ) .then(() => {
-          this.messageService.add({ severity: 'success', summary: 'Email Received', detail: 'Thanks! Your Mail was received successfully.' });
-          this.enquiredMail = '';
-      }) .catch((error) => {
-         this.messageService.add({ severity: 'error', summary: 'Email error', detail: 'Sending failed. Try again.' });
-      });
+    emailjs.send(
+      'service_rk32sy8',
+      'template_4waj1af',
+      templateParams,
+      'zofwWtiXDqaNQCCdM'
+    ).then(() => {
+      this.messageService.add({ severity: 'success', summary: 'Email Received', detail: 'Thanks! Your Mail was received successfully.' });
+      this.enquiredMail = '';
+    }).catch((error) => {
+      this.messageService.add({ severity: 'error', summary: 'Email error', detail: 'Sending failed. Try again.' });
+    });
   }
 
   isValidEmail(email: string): boolean {
-  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-  return emailRegex.test(email);
-} 
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailRegex.test(email);
+  }
 
 }
--- src/app/resume/resume.ts
+++ src/app/resume/resume.ts
@@ -1,12 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { MessageService } from 'primeng/api';
+import emailjs from 'emailjs-com';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-resume',
-  imports: [ButtonModule],
+  imports: [ButtonModule, DialogModule, ReactiveFormsModule,InputTextModule,InputNumberModule,ToastModule],
   templateUrl: './resume.html',
-  styleUrl: './resume.css'
+  styleUrl: './resume.css',
+   providers: [MessageService]
 })
 export class Resume {
+
+  constructor(private fb: FormBuilder,private messageService: MessageService) { }
+
+  isVisibleHireMe: boolean = false;
+  hireForm!: FormGroup;
+
+  ngOnInit() {
+    this.hireForm = this.fb.group({
+      username: ['', Validators.required,Validators.pattern(/^[a-zA-Z\s]{2,50}$/)],
+      number: [null, [Validators.required, Validators.pattern(/^\d{10}$/)]],
+      email: ['', [Validators.required, Validators.email,Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)]]
+    });
+  }
+
+  openHireModal() {
+    this.isVisibleHireMe = true;
+  }
+
+  submitHireForm() {
+    if (this.hireForm.invalid) {
+      this.hireForm.markAllAsTouched();
+      return;
+    }
+
+    console.log('Form Data:', this.hireForm.value);
+
+    // Optionally send data using EmailJS, API, etc.
+
+    this.isVisibleHireMe = false;
+    this.hireForm.reset();
+  }
+
+  closeHireModal() {
+    this.isVisibleHireMe = false;
+    this.hireForm.reset();
+
+  }
 
 }
